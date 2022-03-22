55e39b3c8f9a9d195e70de453aa939dfb9c943fb
Joakim <wahlstrom.joakim@gmail.com>
2022-03-22 14:42:40 +0100

    lektion 1

--- Lektion-1/5-server/index.js
+++ Lektion-1/5-server/index.js
@@ -9,40 +9,80 @@ const server = http.createServer((req, res) => {
     return 
   } 
 
-  res.setHeader('Content-type', 'text/html')
+  // res.setHeader('Content-type', 'text/html')
   
 
-  let fileName;
+  // let fileName;
 
-  switch(req.url) {
+  // switch(req.url) {
 
-    case '/':
-      fileName = 'index.html';
-      break;
-    case '/about':
-      fileName = 'about.html';
-      break;
-    case '/faq':
-      fileName = 'faq.html';
-      break;
-    default:
-      fileName = '404.html'
-      break;
-  }
+  //   case '/':
+  //     fileName = 'index.html';
+  //     res.statusCode = 200;
+  //     break;
+  //   case '/about':
+  //     fileName = 'about.html';
+  //     res.statusCode = 200;
+  //     break;
+  //   case '/about-us':
+  //     res.statusCode = 301;
+  //     res.setHeader('Location', '/about')
+  //     res.end()
+  //     break;
+  //   case '/faq':
+  //     fileName = 'faq.html';
+  //     res.statusCode = 200;
+  //     break;
+  //   default:
+  //     fileName = '404.html'
+  //     res.statusCode = 404;
+  //     break;
+  // }
 
 
-  let filePath = path.join(__dirname, 'views', fileName)
+  // let filePath = path.join(__dirname, 'views', fileName)
+
+  // fs.readFile(filePath, (err, data) => {
+  //   if(err) {
+  //     console.log(err)
+  //     res.end('gick inte')
+  //   }
+
+  //   res.end(data)
+  // })
+
+
+  let filePath = path.join(
+    __dirname,
+    'views',
+    req.url === '/' ? 'index.html' : req.url + '.html'
+  )
 
   fs.readFile(filePath, (err, data) => {
+
     if(err) {
-      console.log(err)
-      res.end('gick inte')
+      if(err.code === 'ENOENT') {
+        fs.readFile('./views/404.html', (err, errData) => {
+          if(err) {
+            console.log(err)
+            res.end()
+            
+          } else {
+            res.statusCode = 404;
+            res.end(errData)
+          }
+        })
+      } else {
+        console.log(err)
+        res.end()
+        return
+      }
+    } else {
+      res.end(data)
+
     }
 
-    res.end(data)
   })
-
-
 
 
 })
