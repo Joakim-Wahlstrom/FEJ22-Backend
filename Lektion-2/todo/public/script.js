00c80032d894f2a99095052a7a2d8e69c7203f64
Joakim <wahlstrom.joakim@gmail.com>
2022-03-24 14:33:32 +0100

    Lektion 2

--- Lektion-2/todo/public/script.js
+++ Lektion-2/todo/public/script.js
@@ -1,5 +1,9 @@
 const noTodosText = document.querySelector('#noTodosText')
 const output = document.querySelector('#output')
+const todoForm = document.querySelector('#todoForm')
+const todoInput = document.querySelector('#todoInput')
+const myModal = document.querySelector('#addTodo')
+const addTodoModal = new bootstrap.Modal(myModal)
 
 let todos = []
 
@@ -38,4 +42,87 @@ const createTodoElement = (todo, parent, placement, isNew) => {
     </div>
   </div>
   `)
+
+  addRemoveOnClick(todo)
+  addToggleComplete(todo)
 }
+
+const addRemoveOnClick = todo => {
+  document.querySelector(`#delete_${todo._id}`).addEventListener('click', () => {
+    deleteTodo(todo)
+  })
+}
+const addToggleComplete = todo => {
+  document.querySelector(`#title_${todo._id}`).addEventListener('click', () => {
+      toggleTodo(todo)
+  })
+}
+
+const toggleTodo = async todo => {
+  const res = await fetch(`http://localhost:8080/api/todos/${todo._id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8'
+    },
+    body: JSON.stringify({
+      completed: !todo.completed
+    })
+  })
+
+  if(!res.ok) return
+
+  const data = await res.json()
+
+  const _todo = document.querySelector(`#todo_${data._id}`)
+  createTodoElement(data, _todo, 'beforebegin', false)
+  _todo.remove()
+}
+
+const deleteTodo = async todo => {
+  const res = await fetch(`http://localhost:8080/api/todos/${todo._id}`, { method: 'DELETE' })
+  if(!res.ok) return
+
+  const data = await res.json()
+
+  const todoElement = document.querySelector(`#todo_${data.id}`)
+  todoElement.addEventListener('animationend', () => {
+    todoElement.remove()
+    todos = todos.filter(todo => todo._id !== data.id)
+    if(todos.length <= 0) {
+      noTodosText.classList.remove('d-none')
+    }
+  })
+
+  todoElement.classList.add('slide-out')
+
+}
+
+const addNewTodo = async title => {
+  const res = await fetch('http://localhost:8080/api/todos', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8'
+    },
+    body: JSON.stringify({ title })
+  })
+
+  const data = await res.json()
+  createTodoElement(data, output, 'beforeend', true)
+  todos.push(data)
+  noTodosText.classList.add('d-none')
+}
+
+todoForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  if(todoInput.value.trim() === '')
+    return
+    
+  addNewTodo(todoInput.value)
+  todoInput.value = ''
+  addTodoModal.hide()
+})
+
+myModal.addEventListener('shown.bs.modal', function () {
+  todoInput.focus()
+})
