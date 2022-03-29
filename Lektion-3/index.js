6e02e7e31ccc304950a1d66d994cdae2b8b93f09
Joakim <wahlstrom.joakim@gmail.com>
2022-03-29 13:46:33 +0200

    lektion 3

---
+++ Lektion-3/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const socket = require('socket.io');
+const http = require('http');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+const PORT = process.env.PORT || 5000
+server.listen(PORT, () => console.log('Server running on http://localhost:' + PORT))
+
+
+const io = socket(server)
+
+
+io.on('connection', socket => {
+  console.log('New user connected', socket.id)
+
+  socket.emit('message', 'Välkommen till chatten')
+
+})
